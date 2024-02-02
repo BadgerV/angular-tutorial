1089058b4214b0c43003904c819919ec76278683
Segunmaru Faozan <segunfaozan@gmail.com>
2024-02-02 21:44:46 +0100

    learned about angular basics, components, angular cli, the file structure, the usage of individual files, data binding, string interpolation, types of data binding, etc

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css',
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [];
+
+  constructor() {
+    
+  }
+}
