a33e5b82108c5a0a53e200fd2336e4fcabc98925
Segunmaru Faozan <segunfaozan@gmail.com>
2024-02-03 15:47:56 +0100

    learned more about data binding, about the Input() and

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../../recipes.model';
 
 @Component({
-  selector: 'app-recipe-item',
-  templateUrl: './recipe-item.component.html',
-  styleUrl: './recipe-item.component.css'
+   selector: 'app-recipe-item',
+   templateUrl: './recipe-item.component.html',
+   styleUrl: './recipe-item.component.css',
 })
 export class RecipeItemComponent {
+   @Output() recipeSelected = new EventEmitter<void>();
 
+   @Input() recipe: Recipe;
+
+   onSelected() {
+      this.recipeSelected.emit();
+   }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipes.model';
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css',
+   selector: 'app-recipe-list',
+   templateUrl: './recipe-list.component.html',
+   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [];
+   @Output() recipeWasSelected = new EventEmitter<Recipe>();
+   recipes: Recipe[] = [
+      new Recipe(
+         'A test recipe',
+         'This is simply a test',
+         'https://www.simplyrecipes.com/thmb/GjaB8D6OyVIAfA_BpGuXr-6eNBU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Baklava-LEAD-11-b2a228e6db9f43d697ae3aed378d0b2a.jpg'
+      ),
+      new Recipe(
+         'Another test recipe',
+         'This is simply a test',
+         'https://www.simplyrecipes.com/thmb/GjaB8D6OyVIAfA_BpGuXr-6eNBU=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Baklava-LEAD-11-b2a228e6db9f43d697ae3aed378d0b2a.jpg'
+      ),
+   ];
 
-  constructor() {
-    
-  }
+   constructor() {}
+
+   onRecipeSelected(recipe: Recipe) {
+      this.recipeWasSelected.emit(recipe);
+   }
 }
