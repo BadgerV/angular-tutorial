9260ebc42b1a355d70859502de81182767504cf0
Segunmaru Faozan <segunfaozan@gmail.com>
2024-02-03 17:47:24 +0100

    learned more stuff about angular

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+   Component,
+   ElementRef,
+   ViewChild,
+   EventEmitter,
+   Output,
+} from '@angular/core';
+import { Ingredient } from '../../shared/ingriedients.model';
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+   selector: 'app-shopping-edit',
+   templateUrl: './shopping-edit.component.html',
+   styleUrl: './shopping-edit.component.css',
 })
 export class ShoppingEditComponent {
+   @ViewChild('nameInput') nameInputRef: ElementRef;
+   @ViewChild('amountInput') amountInputRef: ElementRef;
+   @Output() ingriedientAdded = new EventEmitter<Ingredient>();
 
+   onAddItem() {
+      const ingName = this.nameInputRef.nativeElement.value;
+      const ingAmount = this.amountInputRef.nativeElement.value;
+      const newIngriedient = new Ingredient(ingName, ingAmount);
+      this.ingriedientAdded.emit(newIngriedient);
+   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { Ingredient } from '../shared/ingriedients.model';
 
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css',
+   selector: 'app-shopping-list',
+   templateUrl: './shopping-list.component.html',
+   styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomataoes', 5),
-    new Ingredient('Oranges', 5),
-  ];
+   ingredients: Ingredient[] = [
+      new Ingredient('Apples', 5),
+      new Ingredient('Tomataoes', 5),
+      new Ingredient('Oranges', 5),
+   ];
+
+   onIngredientAdded(ingredient: Ingredient) {
+      this.ingredients.push(ingredient);
+   }
 }
